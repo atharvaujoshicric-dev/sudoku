44468f3966417b05f0ce69baedef9201eb52c5c3
atharvaujoshicric-dev <atharvaujoshicric@gmail.com>
2026-02-28 19:17:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -112,75 +112,76 @@ if st.button("🔄 New Game"):
     st.session_state.completed = False
 
 # ------------------------------
-# OG GRID STYLE
+# Sudoku Grid Styling
 # ------------------------------
 st.markdown("""
 <style>
-.sudoku-grid {
-    display: grid;
-    grid-template-columns: repeat(9, 50px);
-    grid-template-rows: repeat(9, 50px);
-    gap: 0px;
+div[data-testid="column"] > div {
+    display: flex;
+    justify-content: center;
 }
-.cell {
-    width: 50px;
-    height: 50px;
+
+.sudoku-cell input {
     text-align: center;
     font-size: 20px;
-    border: 1px solid #999;
+    height: 45px !important;
+    width: 45px !important;
+    padding: 0 !important;
 }
-.cell input {
-    width: 100%;
-    height: 100%;
-    text-align: center;
+
+.sudoku-fixed {
     font-size: 20px;
-    border: none;
-}
-.bold-right {
-    border-right: 3px solid black !important;
-}
-.bold-bottom {
-    border-bottom: 3px solid black !important;
-}
-.bold-left {
-    border-left: 3px solid black !important;
-}
-.bold-top {
-    border-top: 3px solid black !important;
+    height: 45px;
+    width: 45px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
 """, unsafe_allow_html=True)
 
 user_input = np.zeros((9,9), dtype=int)
 
-st.markdown('<div class="sudoku-grid">', unsafe_allow_html=True)
-
+# ------------------------------
+# Proper 9x9 Grid
+# ------------------------------
 for i in range(9):
+    cols = st.columns(9)
     for j in range(9):
-        value = st.session_state.puzzle[i][j]
-        classes = "cell"
-        if j % 3 == 2 and j != 8:
-            classes += " bold-right"
-        if i % 3 == 2 and i != 8:
-            classes += " bold-bottom"
-        if j == 0:
-            classes += " bold-left"
-        if i == 0:
-            classes += " bold-top"
 
-        if value != 0:
-            st.markdown(f'<div class="{classes}"><b>{value}</b></div>', unsafe_allow_html=True)
-            user_input[i][j] = value
+        # Border logic
+        style = ""
+        if j % 3 == 0:
+            style += "border-left:3px solid black;"
+        if i % 3 == 0:
+            style += "border-top:3px solid black;"
+        if j == 8:
+            style += "border-right:3px solid black;"
+        if i == 8:
+            style += "border-bottom:3px solid black;"
         else:
-            key = f"{i}-{j}"
-            val = st.text_input("", key=key, max_chars=1)
-            if val.isdigit():
-                user_input[i][j] = int(val)
+            style += "border-right:1px solid grey;border-bottom:1px solid grey;"
 
-st.markdown('</div>', unsafe_allow_html=True)
+        value = st.session_state.puzzle[i][j]
+
+        with cols[j]:
+            if value != 0:
+                st.markdown(
+                    f'<div class="sudoku-fixed" style="{style}">{value}</div>',
+                    unsafe_allow_html=True
+                )
+                user_input[i][j] = value
+            else:
+                val = st.text_input(
+                    "",
+                    key=f"{i}-{j}",
+                    max_chars=1
+                )
+                if val.isdigit():
+                    user_input[i][j] = int(val)
 
 # ------------------------------
-# Check
+# Check Solution
 # ------------------------------
 if st.button("✅ Check Solution"):
     if np.array_equal(user_input, st.session_state.solution):
